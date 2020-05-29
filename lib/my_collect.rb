0867c022fc49dcc7f99ac25f2999f7af7f28c780
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-29 16:29:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ new_collection = collection.collect do |value|
    value.upcase
  end
  new_collection
- newer_collection = new_collection.collect do |name|
-   name.split
- end
- newer_collection
+ # newer_collection = new_collection.collect do |name|
+ #   name.split
+ # end
+ # newer_collection
 end
