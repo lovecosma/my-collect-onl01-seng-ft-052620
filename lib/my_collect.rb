2166f19ab408c75a738bf17b7ffa705a24a98d26
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-29 17:27:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(collection)
  if block_given?
  i = 0
  while i < collection.length
-   yield collection[i] {|item| item.upcase}
+   new_collection = yield collection[i] {|item| item.upcase}
    i+=1
  end
-   collection
+   
  else
    puts "Hey! No block was given!"
  end
