26be58100654fa51ef039f3c453f0b8da9da6989
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-29 17:32:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,8 @@ def my_collect(collection)
  i = 0
  new_collection = []
  while i < collection.length
-   new_collection << yield collection[i] 
+   new_collection << yield collection[i]
    i+=1
  end
    new_collection
- else
-   puts "Hey! No block was given!"
- end
  end
