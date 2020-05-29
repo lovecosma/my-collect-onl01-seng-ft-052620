db12a483264b30ea70651190123317972f358593
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-29 17:22:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
  if block_given?
  i = 0
  while i < collection.length
-   yield collection[i] {|item| item.upcase }
+   yield collection[i] {|item| item.upcase}
    i+=1
  end
    collection
